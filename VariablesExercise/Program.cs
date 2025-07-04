eb6fd534cb8666cc72bb0388e60a3f1f302b7815
inkyourshoesgit config user.namegit config --global user.email inkyourshoes@gmail.comglobal user.name inkyourshoes <inkyourshoes@gmail.com>
2025-07-03 21:44:31 -0400

    git add .

--- VariablesExercise/Program.cs
+++ VariablesExercise/Program.cs
@@ -4,7 +4,15 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            string myName = "Edward";
+            int age = 30;
+            double avg = 98.7;
+            decimal bankAccount = 10000.00m;
+            char initial = 'E';
+            bool isMarried = true;
+
+            Console.WriteLine($"Name: {myName}\nAge: {age}\nAverage: {avg}\nMoney: {bankAccount}\nInitial: {initial}\nMarried: {isMarried}");
+             
         }
     }
 }
